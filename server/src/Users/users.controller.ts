50e5ced20da6e147a81d8293abf5d509d14b198e
thupx <xuanthu404@gmail.com>
2019-10-20 18:20:00 +0700

    add test using JwtAuthGuard

--- server/src/Users/users.controller.ts
+++ server/src/Users/users.controller.ts
@@ -3,29 +3,16 @@ import { UserLoginDto } from "./DTOs/userLogin.dto";
 import { UsersService } from "./users.service";
 import { SuccessResponse } from "../Shared/success.dto";
 import { Request, Response } from "express";
+import { UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "../Auth/auth.guard";
 @Controller()
 export class UsersController {
     constructor(private usersService: UsersService) { };
+    @UseGuards(JwtAuthGuard)
     @Get("/users")
     public async getUser(@Req() req: Request, @Res() res: Response) {
         const users = await this.usersService.findAllUsers();
         const payload = new SuccessResponse("All Users", users);
         res.json(payload);
-    }
-    @Post("/users")
-    public async createUser(user) {
-
-    }
-    @Post("/users/login")
-    public async login(@Res() res: Response, @Body() userLoginDto: UserLoginDto) {
-        try {
-            if (userLoginDto) {
-                const { username, password } = userLoginDto;
-                // const user = await this.usersService.login();
-            }
-        }
-        catch (err) {
-
-        }
     }
 }
